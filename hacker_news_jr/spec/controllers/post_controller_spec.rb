2cf7a5593d7613939f75783fa5f76248fba42fb3
Vic <vicxizhu@gmail.com>
2015-07-27 16:25:54 -0400

    inital commit

---
+++ hacker_news_jr/spec/controllers/post_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'post controller tests', type: :controller do
+  # let! (:test_user) { User.create(username: 'tester', password: 'password') }
+
+  #  before :each do
+  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(test_user)
+  #   test_user.posts.create(title: 'google', url: 'www.google.com')
+  # end
+
+  # it 'user can delete their own post' do
+  #   visit "/users/#{test_user.id}"
+  #   click_link 'delete'
+  # end
+
+end
